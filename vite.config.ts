10a106a03fd22b29be3df86688d1899d83b142c7
SISHANT VERMA <zeyotech@gmail.com>
2026-07-24 23:39:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './src/main.tsx',
+      },
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
